fbfd02e096e0bd84930eb856edd3b02dc1f81302
islam <islam@DESKTOP-U88RBMR>
2022-06-08 13:14:10 +0600

    Add project files.

---
+++ WpfApp4/bangladeshi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    class bangladeshi
+    {
+        public string Division1 { get; set; }
+
+        public List<bangladeshi> GetDivision()
+        {
+            List<bangladeshi> bangladeshis = new List<bangladeshi>();
+            
+            return bangladeshis;
+        }
+    }
+}
